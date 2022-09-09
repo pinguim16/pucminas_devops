49a2b83a0724c21dc662f6e98634ce54c9646940
César Augusto <cesaraugustosi16@gmail.com>
2022-09-08 21:49:47 -0300

    Ajuste final

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAX32W6R2LQUG5TKO6"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
--- security_group.tf
+++ security_group.tf
@@ -5,18 +5,9 @@ resource "aws_security_group" "sg-web" {
   vpc_id      = aws_vpc.vpc_teste_pucminas.id
 
   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -24,13 +15,6 @@ resource "aws_security_group" "sg-web" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
